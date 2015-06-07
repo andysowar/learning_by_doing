e5ce8fc2cf8260cc226e5c6a8efe627ecc09ee60
Andy Sowar <andy.sowar@gmail.com>
2015-06-07 12:59:20 -0600

    Fix number_of_seconds.rb
    I was missing a few commas in this file & failed my test.
    
    [#96335298]

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -14,8 +14,8 @@ puts "There are #{minutes_per_hour} minutes in an hour"
 puts "There are #{hours_per_day} hours in a day"
 puts "There are #{days_per_week} days in a week"
 puts 'That means there are:'
-puts "  #{seconds_per_hour} seconds in an hour"
-puts "  #{seconds_per_day} seconds in an day"
+puts "  #{seconds_per_hour} seconds in an hour,"
+puts "  #{seconds_per_day} seconds in an day,"
 puts "  #{seconds_per_week} seconds in a week"
 puts "That means when you turn 20, you've been alive for "\
 "#{seconds_per_year * 20} seconds,"
