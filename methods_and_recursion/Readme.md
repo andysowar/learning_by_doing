68458b77a73f7e706a440996b9623252eac66a48
Andy Sowar <andy.sowar@gmail.com>
2015-06-11 21:31:49 -0600

    Add mad libs homework
    [#96846876]

---
+++ methods_and_recursion/Readme.md
@@ -0,0 +1,7 @@
+#Mad Libs
+
+Create a program called "mad_libs.rb".
+
+• Download http://www.madlibs.com/assets/0000/1042/Vac ationFun_ML_2009_pg15.pdf
+
+• Ask the user for each fill in the blank • Output the finished Mad Lib®
---
+++ methods_and_recursion/mad_libs.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+puts 'Enter an adjective.'
+adjective_1 = gets.chomp
+puts 'Enter another adjective.'
+adjective_2 = gets.chomp
+puts 'Enter a noun.'
+noun_1 = gets.chomp
+puts 'Enter another noun.'
+noun_2 = gets.chomp
+puts 'Enter a plural noun.'
+plural_noun_1 = gets.chomp
+puts 'Enter a game you like to play.'
+game = gets.chomp
+puts 'Enter a plural noun.'
+plural_noun_2 = gets.chomp
+puts 'Enter a verb ending in "ing".'
+verb_1 = gets.chomp
+puts 'Enter another verb ending in "ing"'
+verb_2 = gets.chomp
+puts 'Enter a plural noun'
+plural_noun_3 = gets.chomp
+puts 'Enter a verb ending in "ing"'
+verb_3 = gets.chomp
+puts 'Enter a noun'
+noun_3 = gets.chomp
+puts 'Enter the name of a plant'
+plant = gets.chomp
+puts 'Name a part of the body'
+body_part = gets.chomp
+puts 'Enter the name of a place'
+place = gets.chomp
+puts 'Enter a verb ending in "ing"'
+verb_4 = gets.chomp
+puts 'Enter an adjective'
+adjective_3 = gets.chomp
+puts 'Enter a number'
+number = gets.chomp.to_s
+puts 'Enter a plural noun'
+plural_noun_4 = gets.chomp
+
+puts "A vacation is when you take a trip to some #{adjective_1} place"
+puts "with your #{adjective_2} family. Usually you go to some place"
+puts "that is near a/an #{noun_1} or up on a/an #{noun_2}."
+puts "A good vacation place is one where you can ride #{plural_noun_1}"
+puts "or play #{game} or go hunting for #{plural_noun_2}. I like"
+puts "to spend my time #{verb_1} or #{verb_2}."
+puts 'When parents go on a vacation, they spend their time eating'
+puts "three #{plural_noun_3} a day, and fathers play golf, and mothers"
+puts "sit around #{verb_3}. Last summer, my little brother"
+puts "fell in a/an #{noun_3} and got poison #{plant} all"
+puts "over his #{body_part}. My family is going to go to (the)"
+puts "#{place}, and I will practice #{verb_4}. Parents"
+puts 'need vacations more than kids because parents are always very'
+puts "#{adjective_3} and because they have to work #{number}"
+puts "hours every day all year making enough #{plural_noun_4} to pay"
+puts 'for the vacation.'
