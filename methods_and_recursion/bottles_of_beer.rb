01b7725bc78f2be9fa71f968050fc0b00e06d6af
Andy Sowar <andy.sowar@gmail.com>
2015-06-13 17:39:59 -0600

    Made corrections on homework
    Removed return form pluralize method & made it display number along with bottles.
    Added if/end statement to bottles method.
    
    [#96848518]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -2,15 +2,18 @@
 
 def pluralize(number, string)
   string += 's' unless number == 1
-  return string
+  "#{number} #{string}"
 end
 
 def bottles(beer)
-  puts "No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall." if beer == 0
-  return nil if beer == 0
-  puts "#{beer} #{pluralize(beer, 'bottle')} of beer on the wall, #{beer} #{pluralize(beer, 'bottle')} of beer."
-  puts "Take one down and pass it around, #{beer - 1} #{pluralize(beer - 1, 'bottle')} of beer on the wall."
+  if beer == 0
+    puts 'No more bottles of beer on the wall, no more bottles of beer.'
+    puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+    return nil
+  end
+  print "#{pluralize(beer, 'bottle')} of beer on the wall,"
+  puts " #{pluralize(beer, 'bottle')} of beer."
+  puts "Take one down and pass it around, #{pluralize(beer - 1, 'bottle')} of beer on the wall."
   puts ''
   bottles(beer - 1)
 end
