bcbbcbf5488e9b3a1459cbeb460b189c2f33bfcd
Andy Sowar <andy.sowar@gmail.com>
2015-06-13 16:45:22 -0600

    Update to use arrays for questions & answers
    [#96846876]

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -1,57 +1,34 @@
 #!/usr/bin/env ruby
 
-puts 'Enter an adjective.'
-adjective_1 = gets.chomp
-puts 'Enter another adjective.'
-adjective_2 = gets.chomp
-puts 'Enter a noun.'
-noun_1 = gets.chomp
-puts 'Enter another noun.'
-noun_2 = gets.chomp
-puts 'Enter a plural noun.'
-plural_noun_1 = gets.chomp
-puts 'Enter a game you like to play.'
-game = gets.chomp
-puts 'Enter a plural noun.'
-plural_noun_2 = gets.chomp
-puts 'Enter a verb ending in "ing".'
-verb_1 = gets.chomp
-puts 'Enter another verb ending in "ing"'
-verb_2 = gets.chomp
-puts 'Enter a plural noun'
-plural_noun_3 = gets.chomp
-puts 'Enter a verb ending in "ing"'
-verb_3 = gets.chomp
-puts 'Enter a noun'
-noun_3 = gets.chomp
-puts 'Enter the name of a plant'
-plant = gets.chomp
-puts 'Name a part of the body'
-body_part = gets.chomp
-puts 'Enter the name of a place'
-place = gets.chomp
-puts 'Enter a verb ending in "ing"'
-verb_4 = gets.chomp
-puts 'Enter an adjective'
-adjective_3 = gets.chomp
-puts 'Enter a number'
-number = gets.chomp.to_s
-puts 'Enter a plural noun'
-plural_noun_4 = gets.chomp
+answers = Array.new
 
-puts "A vacation is when you take a trip to some #{adjective_1} place"
-puts "with your #{adjective_2} family. Usually you go to some place"
-puts "that is near a/an #{noun_1} or up on a/an #{noun_2}."
-puts "A good vacation place is one where you can ride #{plural_noun_1}"
-puts "or play #{game} or go hunting for #{plural_noun_2}. I like"
-puts "to spend my time #{verb_1} or #{verb_2}."
+questions = ["Enter an adjective", "Enter an adjective", "Enter a noun", "Enter a noun", "Enter a plural noun",
+  "Enter a game you like to play", "Enter a plural noun", "Enter a verb ending in -ing ", "Enter a verb ending in -ing",
+  "Enter a plural noun", "Enter a verb ending in -ing ","Enter a noun", "Enter the name of a plant",
+  "Enter a body part","Enter a place you like to visit","Enter a verb ending in -ing ","Enter an adjective",
+  "Enter a number","Enter a plural noun"]
+
+questions.each do |question, input|
+puts question
+input = gets.chomp
+answers.push input
+end
+
+puts 'Thanks! Here is your mad lib:'
+
+puts "A vacation is when you take a trip to some #{answers[0]} place"
+puts "with your #{answers[1]} family. Usually you go to some place"
+puts "that is near a/an #{answers[2]} or up on a/an #{answers[3]}."
+puts "A good vacation place is one where you can ride #{answers[4]}"
+puts "or play #{answers[5]} or go hunting for #{answers[6]}. I like"
+puts "to spend my time #{answers[7]} or #{answers[8]}."
 puts 'When parents go on a vacation, they spend their time eating'
-puts "three #{plural_noun_3} a day, and fathers play golf, and mothers"
-puts "sit around #{verb_3}. Last summer, my little brother"
-puts "fell in a/an #{noun_3} and got poison #{plant} all"
-puts "over his #{body_part}. My family is going to go to (the)"
-puts "#{place}, and I will practice #{verb_4}. Parents"
+puts "three #{answers[9]} a day, and fathers play golf, and mothers"
+puts "sit around #{answers[10]}. Last summer, my little brother"
+puts "fell in a/an #{answers[11]} and got poison #{answers[12]} all"
+puts "over his #{answers[13]}. My family is going to go to (the)"
+puts "#{answers[14]}, and I will practice #{answers[15]}. Parents"
 puts 'need vacations more than kids because parents are always very'
-puts "#{adjective_3} and because they have to work #{number}"
-puts "hours every day all year making enough #{plural_noun_4} to pay"
+puts "#{answers[16]} and because they have to work #{answers[17]}"
+puts "hours every day all year making enough #{answers[18]} to pay"
 puts 'for the vacation.'
