6568dbd2feea1e632f89e9f1821a52b539675ed2
Andy Sowar <andy.sowar@gmail.com>
2015-06-12 18:19:08 -0600

    Fix last two lines of song
    [#96848518]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -6,6 +6,8 @@ def pluralize(number, string)
 end
 
 def bottles(beer)
+  puts "No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall." if beer == 0
   return nil if beer == 0
   puts "#{beer} #{pluralize(beer, 'bottle')} of beer on the wall, #{beer} #{pluralize(beer, 'bottle')} of beer."
   puts "Take one down and pass it around, #{beer - 1} #{pluralize(beer - 1, 'bottle')} of beer on the wall."
