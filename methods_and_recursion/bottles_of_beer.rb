c9924533701863e16230e08d7e0774930d087d51
Andy Sowar <andy.sowar@gmail.com>
2015-06-12 18:03:42 -0600

    Update homework to work recursively & add pluralize method
    [#96848518]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/env ruby
 
-def bottles_of_beer(number = 99)
-  while number > 0
-  puts "#{number.to_s} bottles of beer on the wall, #{number.to_s} bottles of beer."
-  number = number - 1
-  puts "Take one down and pass it around, #{number.to_s} bottles of beer on the wall."
-  puts ''
-    if number == 0
-      break
-    end
-  end
-
+def pluralize(number, string)
+  string += 's' unless number == 1
+  return string
 end
 
-bottles_of_beer
+def bottles(beer)
+  return nil if beer == 0
+  puts "#{beer} #{pluralize(beer, 'bottle')} of beer on the wall, #{beer} #{pluralize(beer, 'bottle')} of beer."
+  puts "Take one down and pass it around, #{beer - 1} #{pluralize(beer - 1, 'bottle')} of beer on the wall."
+  puts ''
+  bottles(beer - 1)
+end
+
+bottles(99)
