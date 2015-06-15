6b14a6a8990931552351cdfeaf23767616ee17cb
Andy Sowar <andy.sowar@gmail.com>
2015-06-14 20:21:20 -0600

    First working version
    [#96957908]

---
+++ objects/cash_register.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+class CashRegister
+
+  def initialize
+    @total_amount_owed = 0.00
+  end
+
+  def purchase(items_purchased)
+      puts items_purchased
+      @total_amount_owed += items_purchased
+  end
+
+
+  def total
+    @total_amount_owed
+  end
+
+  # returns change
+  def pay(amount_paid)
+    if @total_amount_owed > amount_paid
+       @total_amount_owed -= amount_paid
+       puts "Your new total is #{@total_amount_owed}"
+    else
+      change = amount_paid - @total_amount_owed
+      puts "Your change is $#{change.round(2)}"
+      @total_amount_owed = 0.00
+    end
+  end
+
+end
+
+register = CashRegister.new
+  puts 'Ex. #1'
+  puts register.total
+  register.purchase(3.78)
+  puts register.total
+  register.pay(5.00)
+  puts register.total
+  puts ''
+
+  puts 'Ex. #2'
+  register = CashRegister.new
+  puts register.total
+  register.purchase(3.78)
+  register.purchase(5.22)
+  puts register.total
+  register.pay(5.00)
+  puts register.total
+  register.pay(5.00)
+  puts register.total
