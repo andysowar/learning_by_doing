148a7c922f3217b723468ff2d9a4be55a65f1d8a
Andy Sowar <andy.sowar@gmail.com>
2015-06-19 16:12:23 -0600

    Completed Version 1.0
    [#96957926]

---
+++ objects/driving.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+#Calculates fuel cost for your road trip.
+class FuelCost
+  def initialize
+    @miles_per_gallon = 0
+    @total_miles = 0
+    @price_per_gallon = 0
+  end
+
+  def length_of_trip(miles_driven)
+    @total_miles += miles_driven
+  end
+
+  def mpg(miles_per_gallon)
+    @miles_per_gallon = miles_per_gallon
+  end
+
+  def gallons_of_gas
+    @gallons_needed = @total_miles / @miles_per_gallon
+    puts "Total miles: #{@total_miles}"
+    print "You need #{@gallons_needed} gallons of gas"
+    puts " for your trip."
+  end
+
+  def fuel_cost(price_per_gallon)
+    @total_fuel_cost = price_per_gallon * @gallons_needed
+    print "At $#{format("%.2f", price_per_gallon)} per gallon, you will need"
+    puts " $#{format("%.2f", @total_fuel_cost)} to pay for gas!"
+  end
+end
+
+road_trip = FuelCost.new
+road_trip.length_of_trip(300)
+road_trip.length_of_trip(375)
+road_trip.mpg(25)
+road_trip.gallons_of_gas
+road_trip.fuel_cost(2.60)
+
+#Divide length of trip by cars MPG. This will determine amount of gas needed.
+#Multiply those results by the fuel_price to get total cost of gas.
+
